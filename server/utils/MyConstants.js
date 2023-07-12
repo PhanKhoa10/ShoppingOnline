d0138a0272c82500f9104514bd9e4b608eef250b
PhanKhoa10 <khoa.dev22@gmail.com>
2023-07-12 14:02:59 +0700

    admin

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonline.dcbn8en.mongodb.net',
+    DB_USER: 'khoadev22',
+    DB_PASS: '12345',
+    DB_DATABASE: 'ShoppingOnline',
+    EMAIL_USER: 'khoa.dev22@gmail.com', // gmail service
+    EMAIL_PASS: 'vzvgzqhhhjegbkho',
+    JWT_SECRET: '12345',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  
+  module.exports = MyConstants;
